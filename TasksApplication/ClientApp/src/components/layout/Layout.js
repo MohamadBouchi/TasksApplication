a3ed9b14e21ac9ed9645b036a685ecbf6a2d9d9b
mohamedsubhi <mohamedsubhibouchi@gmail.com>
2019-03-21 16:14:36 +0100

    adding some cssto make document and container height 100%

--- TasksApplication/ClientApp/src/components/layout/Layout.js
+++ TasksApplication/ClientApp/src/components/layout/Layout.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import NavMenu from '../navigation/NavMenu';
+import './layout.css';
 
 export default props => (
-  <div>
+  <div id="content">
     <NavMenu />
     <Container>
       {props.children}
